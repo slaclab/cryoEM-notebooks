7755e26b201dbb9591b34df69e6bcb1192ab7746
fredericpoitevin <frederic.poitevin@gmail.com>
2019-06-24 14:51:14 -0700

    started imutils.py to handle binning and normalization. Binning with skimage.transform added, test with DM on particles notebook (still need to refine parameters). Started OSF notebook.

--- src/imutils.py
+++ src/imutils.py
@@ -1,4 +1,34 @@
 # 
 # all utilities to handle images: binning, normalization, ...
 #
-
+import numpy as np
+import skimage
+#
+def binstack(imstack, bin_size=2, method='skimage'):
+	""" binning: provides several approaches to binning images in a stack
+	
+	INPUTS:
+	======
+	- imstack  : 
+	first axis gives image index in stack.
+	- bin_size : 
+	reducing factor in one direction. 
+	- method   : 
+	. 'skimage' 
+	"""
+	L, Nrow, Ncol = imstack.shape
+	# make sure the new image size is a multiple of 2
+	newNrow = 2*int(np.ceil((Nrow/bin_size)/2))
+	newNcol = 2*int(np.ceil((Ncol/bin_size)/2))
+	#
+	if(method=='skimage'):
+		binned_imstack = np.zeros((imstack.shape[0],newNrow,newNcol))
+		for i in np.arange(imstack.shape[0]):
+			binned_imstack[i,...] = skimage.transform.resize(imstack[i,...], 
+                                                                         output_shape=(newNrow,newNcol), 
+                                                                         order=1,
+                                                                         mode='edge',
+                                                                         preserve_range=True,
+                                                                         anti_aliasing=True)
+	return binned_imstack
+#
