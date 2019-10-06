86d38d0e12451dcf7d1ffb5adefcef38eca4b178
Frederic Poitevin <frederic.poitevin@gmail.com>
2019-10-06 16:38:34 -0700

    moved star_reader from relion_reader to cryoemio. deleted relion_reader. started writing star2hdf5 in cryoemio.

--- src/cryoemio.py
+++ src/cryoemio.py
@@ -2,6 +2,103 @@ import numpy as np
 import h5py
 import os
 import mrcfile
+import CifFile
+from CifFile import ReadCif
+
+## RELION related ##
+
+def star_reader(filename):
+    """
+    star_reader: reads a star file and returns a dictionary
+    """
+    key='data_'
+    new_key='data_metadata'
+    new_starfile='.tmp.star'
+    #
+    with open(filename) as f1:
+        with open(new_starfile, 'w') as f2:
+            lines = f1.readlines()
+            for line in lines:
+                if(line.startswith(key)):
+                    f2.write(new_key)
+                else:
+                    f2.write(line)
+    data = ReadCif(new_starfile, grammar='STAR2')
+    os.remove(new_starfile)
+    print('Number of particles in this star file: {}'.format(len(data['metadata'][data['metadata'].keys()[0]])))
+    print("The entries in the returned dictionary are:")
+    print("data['metadata'].keys(): {}".format(data['metadata'].keys()))
+    return data
+
+def star2np(filename, key, keytype=float):
+    """
+    star2np: returns an item from a star file as a numpy array
+    """
+    data  = star_reader(filename)
+    array = starkey2np(data, key, keytype=keytype)
+    return array
+
+def starkey2np(data, key, keytype=float):
+    """
+    starkey2np: star file has already been read.
+    """
+    if key in data['metadata']:
+        array = np.array(data['metadata'][key]).astype(keytype)
+    return array
+
+def star2hdf5(starfile, hdf5file, path_to_mrcs):
+    """
+    """
+    data = star_reader(starfile)
+    with h5py.File(h5_file, 'w') as hf:
+        i=0
+        for particle in data['metadata']['_rlnimagename']:
+            print(particle)
+            # retrieve the particle image
+            frame, relpath = particle.split('@')
+            iframe = np.int(frame)
+            mrcs_filename = relpath.split('/')[-1]
+            mrcs_file = path_to_mrcs+mrcs_filename
+            mrcs_data = mrc2data(mrc_file = mrcs_file)
+            image = mrcs_data[iframe-1,...]
+            # retrieve the metadata
+            ### 2D class metadata
+            class_number = data['metadata']['_rlnclassnumber'][i]
+            inplane_rotation = data['metadata']['_rlnanglepsi'][i]
+            ### CTF related metada
+            ctf_zu = data['metadata']['_rlndefocusu'][i]
+            ctf_zv = data['metadata']['_rlndefocusv'][i]
+            defocus = (ctf_zu + ctf_zv)/2.0
+            #
+            if(i==0):
+                hf.create_dataset('particles', 
+                                   data=image,
+                                   maxshape=(None,image.shape[0],image.shape[1]), 
+                                   chunks=True)
+                hf.create_dataset('defocus',
+                                   data = defocus,
+                                   maxshape=(None,1),
+                                   chunks=True)
+                hf.create_dataset('2dclass',
+                                   data = class_number,
+                                   maxshape=(None,1),
+                                   chunks=True)
+                hf.create_dataset('in-plane_rotation',
+                                  data = inplane_rotation,
+                                  maxshape=(None,1),
+                                  chunks=True)
+            else:
+                hf['particles'].resize( (hf['particles'].shape[0] + 1), axis=0 )
+                hf['particles'][-1:] = image
+                hf['defocus'].resize( (hf['defocus'].shape[0] + 1), axis=0 )
+                hf['defocus'][-1:] = defocus
+                hf['2dclass'].resize( (hf['2dclass'].shape[0] + 1), axis=0 )
+                hf['2dclass'][-1:] = defocus
+                hf['in-plane_rotation'].resize( (hf['in-plane_rotation'].shape[0] + 1), axis=0 )
+                hf['in-plane_rotation'][-1:] = defocus
+            i+=1
+
+##
 
 def simio(pdbdir, pdb_keyword, mrcdir, mrc_keyword, action='define'):
     """ simio
@@ -20,97 +117,97 @@ def simio(pdbdir, pdb_keyword, mrcdir, mrc_keyword, action='define'):
                 os.remove(f)
 
 def mrc2data(mrc_file = None):
-	""" mrc2data
-	"""
-	if mrc_file is not None:
-		with mrcfile.open(mrc_file, 'r+', permissive=True) as mrc:
-                        micrograph = mrc.data
-		if(len(micrograph.shape)==2):
-			micrograph = micrograph[np.newaxis,...]
-		return micrograph
+    """ mrc2data
+    """
+    if mrc_file is not None:
+        with mrcfile.open(mrc_file, 'r+', permissive=True) as mrc:
+            micrograph = mrc.data
+        if(len(micrograph.shape)==2):
+            micrograph = micrograph[np.newaxis,...]
+        return micrograph
 
 ###### / dictionary approach
 def mrc2dic2hdf5(mrc_file = None, h5_file = None, dic = None):
-	""" mrc2hdf5
-	"""
-	if mrc_file is not None:
-		micrograph = mrc2data(mrc_file = mrc_file)
-		if dic is None:
-			dic = {}
-		dic['data'] = micrograph
-		save_dict_to_hdf5(dic, h5_file)
+    """ mrc2hdf5
+    """
+    if mrc_file is not None:
+        micrograph = mrc2data(mrc_file = mrc_file)
+        if dic is None:
+            dic = {}
+        dic['data'] = micrograph
+        save_dict_to_hdf5(dic, h5_file)
 
 def data_and_dic_2hdf5(data, h5_file, dic = None):
-	"""
-	"""
-	if dic is None:
-		dic = {}
-	dic['data'] = data
-	save_dict_to_hdf5(dic, h5_file)
+    """
+    """
+    if dic is None:
+        dic = {}
+    dic['data'] = data
+    save_dict_to_hdf5(dic, h5_file)
 
 ############# / specific to TEM-simulator
 def add_crd_to_dic(crd_file = None, dic = None):
-	""" add_crd_to_dic: [specific to TEM-simulator input/output]
-	this function reads the text content of a coordinates file and adds it to the input dictionary.
-	Returns the richer dictionary.
-	"""
-	if dic is None:
-		dic = {}
-	if crd_file is not None:
-		crd = np.genfromtxt(crd_file, skip_header=3)
-		dic['coordinates'] = crd
-	return dic
+    """ add_crd_to_dic: [specific to TEM-simulator input/output]
+    this function reads the text content of a coordinates file and adds it to the input dictionary.
+    Returns the richer dictionary.
+    """
+    if dic is None:
+        dic = {}
+    if crd_file is not None:
+        crd = np.genfromtxt(crd_file, skip_header=3)
+        dic['coordinates'] = crd
+    return dic
 #
 def add_crd_to_h5(input_h5file = None, input_crdfile = None, output_h5file = None):
-	""" add_crd_to_h5: [specific to TEM-simulator input/output]
-	this function assumes an .hdf5 file containing particle images in a dictionary. Potentially some other info.
-	We want to add a field here where the particle coordinates read from the crd file are added.
-	"""
-	if input_h5file is not None:
-		dic = load_dict_from_hdf5(input_h5file)
-	else:
-		dic = {}
-	if input_crdfile is not None:
-		crd = np.genfromtxt(input_crdfile, skip_header=3)
-		dic['coordinates'] = crd
-		save_dict_to_hdf5(dic, output_h5file)
-	else:
-		print("Error")
+    """ add_crd_to_h5: [specific to TEM-simulator input/output]
+    this function assumes an .hdf5 file containing particle images in a dictionary. Potentially some other info.
+    We want to add a field here where the particle coordinates read from the crd file are added.
+    """
+    if input_h5file is not None:
+        dic = load_dict_from_hdf5(input_h5file)
+    else:
+        dic = {}
+    if input_crdfile is not None:
+        crd = np.genfromtxt(input_crdfile, skip_header=3)
+        dic['coordinates'] = crd
+        save_dict_to_hdf5(dic, output_h5file)
+    else:
+        print("Error")
 ############ specific to TEM-simulator / #####
 ##### dictionary approach / ####
 
-def  mrclist2hdf5( mrc_list=None, h5_file=None , verbose=False):
-	"""
-	"""
-	if h5_file is not None:
-		with h5py.File(h5_file, 'w') as hf:
-			i=0
-			if mrc_list is not None:
-				for mrc in mrc_list:
-					new_data = mrc2data(mrc_file = mrc)
-					if verbose:
-						print('{0} {1}'.format(mrc,new_data.shape))
-					if(i==0):
-						hf.create_dataset('particles', data = new_data, maxshape=(None,new_data.shape[1],new_data.shape[2]), chunks=True)
-					else:
-						hf['particles'].resize( (hf['particles'].shape[0] + new_data.shape[0]), axis=0 )
-						hf['particles'][-new_data.shape[0]:] = new_data
-					i+=1
+def mrclist2hdf5( mrc_list=None, h5_file=None , verbose=False):
+    """
+    """
+    if h5_file is not None:
+        with h5py.File(h5_file, 'w') as hf:
+            i=0
+            if mrc_list is not None:
+                for mrc in mrc_list:
+                    new_data = mrc2data(mrc_file = mrc)
+                    if verbose:
+                        print('{0} {1}'.format(mrc,new_data.shape))
+                    if(i==0):
+                        hf.create_dataset('particles', data=new_data,maxshape=(None,new_data.shape[1],new_data.shape[2]), chunks=True)
+                    else:
+                        hf['particles'].resize( (hf['particles'].shape[0] + new_data.shape[0]), axis=0 )
+                        hf['particles'][-new_data.shape[0]:] = new_data
+                    i+=1
 
 def loadhdf5(h5_file, verbose=False):
-	""" 
-	...
-	"""
-	data = h5py.File(h5_file,'r')
-	if verbose:
-		print('List of datasets in .hdf5 file:')
-		data.visititems(print_attrs)
-	return data
+    """ 
+    ...
+    """
+    data = h5py.File(h5_file,'r')
+    if verbose:
+        print('List of datasets in .hdf5 file:')
+        data.visititems(print_attrs)
+    return data
 
 def print_attrs(name, obj):
-	print( name )
-	for key, val in obj.attrs.items():
-		print("    %s: %s" % (key, val))
+    print( name )
+    for key, val in obj.attrs.items():
+        print("    %s: %s" % (key, val))
 
 
 # The following hdf5 save and load functions are taken from here: 
